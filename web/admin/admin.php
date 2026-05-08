5515b752a333d9e223fc7eb5f658272de6bf7aea
a25adrtomdie <a25adrtomdie@inspedralbes.cat>
2026-05-08 10:05:59 +0200

    Lista de incidencias pendientes

--- web/admin/admin.php
+++ web/admin/admin.php
@@ -5,7 +5,7 @@
         <div class="d-grid gap-4">
             <a href="listTecnics.php" class="btn rounded text-white btn-index" style="background-color:#278DE6">TROBA UN TÈCNIC</a>
             <a href="listIncidAdmin.php" class="btn rounded text-white btn-index" style="background-color:#278DE6">LLISTAR INCIDÈNCIES</a>
-            <a href="" class="btn rounded text-white btn-index" style="background-color:#278DE6">ASSIGNAR INCIDÈNCIES</a>
+            <a href="listIncidPendents.php" class="btn rounded text-white btn-index" style="background-color:#278DE6">ASSIGNAR INCIDÈNCIES</a>
         </div>
     </div>
     <a href="../index.php" class="btn rounded text-white btn-index" style="background-color:#278DE6">INICI</a>
